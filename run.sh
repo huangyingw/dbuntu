83e42cad42c62be22db4a75ff8d65e2fe30a2836
Ying Huang <yinghuang@itutorgroup.com>
2016-05-20 15:46:30 -0700

    n

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+imageName=huangyingw/dbuntu
+for CONTAINERID in $(docker ps|grep "$imageName"|awk '{print $1}' )
+do
+  docker stop "$CONTAINERID"
+  docker rm "$CONTAINERID"
+done
+docker build -f Dockerfile -t "$imageName" .
+docker run \
+  --net=host \
+  -it "$imageName" bash
+
+cd -
